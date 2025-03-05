26d8d7fef91bcfe546ec9cad25ed6aa1dd8191d1
Abishekpriyan <123065314+Abishekpriyan@users.noreply.github.com>
2025-03-05 17:49:50 +0530

    Delete Project directory

--- Project/HTML/navbar.js
+++
@@ -1,6 +0,0 @@
-// Load navbar.html into the #navbar div
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("home.html")
-        .then(response => response.text())
-        .then(data => document.getElementById("navbar").innerHTML = data);
-});
